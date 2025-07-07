fc52261b2415f11678e33f34eee3bcbbaa676a19
Yunita Nur <113296137+YunitaNur0706@users.noreply.github.com>
2025-07-07 17:21:50 +0700

    1 streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+st.set_page_config(layout="wide")
+st.title("Aplikasi Analisis Kemiskinan Indonesia dengan Elastic Net Regression")
